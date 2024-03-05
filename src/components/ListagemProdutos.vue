d2419af1b920da72e86be2cb380a992e7447bcff
brunahenz <brunaehenz@gmail.com>
2024-03-05 16:46:39 -0300

    composables + listagem de produtos

--- src/components/ListagemProdutos.vue
+++ src/components/ListagemProdutos.vue
@@ -1,6 +1,11 @@
 <script setup>
 import { ref, onMounted } from 'vue';
 import axios from 'axios';
+import { useScreen } from '@/composables/screen';
+
+const { browserWidth, deviceWidth, isMobile } = useScreen();
+
+
 const produtos = ref([]);
 
 onMounted(async () => {
@@ -13,7 +18,13 @@ const formatPrice = (price) => `R$ ${price.toFixed(2).replace('.', ',')}`;
 
 <template>
   <div>
-    <h1>Produtos</h1>
+    <div>
+      <h1>
+        Produtos - {{ browserWidth }} - {{ deviceWidth }} - {{
+          isMobile }}
+        <span v-if="isMobile">É móvel</span>
+      </h1>
+    </div>
     <div class="container">
       <div class="card" v-for="produto in produtos" :key="produto.id">
         <h1 class="card--title">{{ produto.title }}</h1>
@@ -25,54 +36,59 @@ const formatPrice = (price) => `R$ ${price.toFixed(2).replace('.', ',')}`;
   </div>
 </template>
 
-<style scoped > @media (max-width: 768px) {
-  .container {
-    gap: 0.5rem;
-  }
-  .card {
-    width: 92%;
-  }
-}
+<style scoped>
+ @media (max-width: 768px) {
+   .container {
+     gap: 0.5rem;
+   }
 
-@media (min-width: 768px) and (max-width: 1024px) {
-  .card {
-    width: 22rem;
-  }
-}
+   .card {
+     width: 92%;
+   }
+ }
 
-.container {
-  display: flex;
-  flex-wrap: wrap;
-  gap: 1rem;
-  justify-content: center;
-  align-items: center;
-  margin: auto;
-  padding: 1rem 0;
-}
-.card {
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-  flex-direction: column;
-  width: 15rem;
-  height: 25rem;
-  background: #fff;
-  box-shadow: 0 10px 20px rgba(0, 0, 0, 0.19), 0 6px 6px rgba(0, 0, 0, 0.23);
-  border-radius: 10px;
-  margin: auto;
-  overflow: hidden;
-}
-.card--avatar {
-  width: 100%;
-  height: 17rem;
-  object-fit: cover;
-  margin-bottom: 0.5rem;
-}
-.card--title {
-  color: #222;
-  font-weight: 700;
-  text-transform: capitalize;
-  font-size: 1.1rem;
-  margin-top: 0.5rem;
-}
+ @media (min-width: 768px) and (max-width: 1024px) {
+   .card {
+     width: 22rem;
+   }
+ }
+
+ .container {
+   display: flex;
+   flex-wrap: wrap;
+   gap: 1rem;
+   justify-content: center;
+   align-items: center;
+   margin: auto;
+   padding: 1rem 0;
+ }
+
+ .card {
+   display: flex;
+   align-items: center;
+   justify-content: space-between;
+   flex-direction: column;
+   width: 15rem;
+   height: 25rem;
+   background: #fff;
+   box-shadow: 0 10px 20px rgba(0, 0, 0, 0.19), 0 6px 6px rgba(0, 0, 0, 0.23);
+   border-radius: 10px;
+   margin: auto;
+   overflow: hidden;
+ }
+
+ .card--avatar {
+   width: 100%;
+   height: 17rem;
+   object-fit: cover;
+   margin-bottom: 0.5rem;
+ }
+
+ .card--title {
+   color: #222;
+   font-weight: 700;
+   text-transform: capitalize;
+   font-size: 1.1rem;
+   margin-top: 0.5rem;
+ }
 </style>
